bc45a494c91c19525ba756625072949ed656b3a9
Martin Massé <martin@nuages.org>
2021-05-26 09:35:27 -0400

    ok

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,15 +1,3 @@
-<div>
-
-    <span class="text-3xl font-bold"> 
-    </span>
-    <span>
-
-    </span>
-    <span>
-         </span>
-
-</div>
-
 
 <main class="mt-16 mx-auto max-w-7xl px-4 sm:mt-24">
     <div class="text-center">
@@ -18,28 +6,45 @@
             <span class="block text-indigo-600 xl:inline"></span>
           </h1>
       <h1 class="text-2xl tracking-tight font-extrabold text-gray-900 sm:text-3xl md:text-4xl">
-        <span class="block xl:inline">.NET</span>
-        <span class="block text-indigo-600 xl:inline">Cloud development</span>
+       
+        <span class="block text-indigo-600 xl:inline">Développement Cloud</span>  <span class="block xl:inline">avec .NET Core</span>
       </h1>
       <p class="mt-5 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-7 md:text-xl md:max-w-3xl">
-        I provide full development services for the .NET stack. I have almost 20 year of experience with ASP.NET/C#/SQL Server and 7 years of deployment on AWS. I'm seeking new software development partnership opportunities.
+        Je fournis des services de développement complets pour la Stack .NET. J'ai 20 ans d'expérience avec ASP.NET/C#/SQL Server et 7 ans de déploiement sur AWS. 
+      </p>
+      <p class="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
+        Je cherche de nouvelles <span class="font-bold">opportunités de partenariat</span>  pour le développement de solutions Cloud.
 
       </p>
       <p class="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
-        Contact me if you have a project idea and need someone to make it happen.
-
+        Contactez-moi si vous avez une idée de projet et que vous avez besoin de quelqu'un pour vous aider à réaliser votre projet.
       </p>
+    
+   
       <!-- <p class="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-sm md:mt-5 md:text-lg md:max-w-3xl">
         ASP.NET, ASP.NET Core, C# , SignalR, EF, MVC, xUnit, Moq, EC2, ELB, EB, S3, SQS, SNS, SES, RDS, Elasticache (REDIS), Lambda, CloudWatch, CloudFront, CodeBuild, CodePipeline, Route 53, API Gateway, SQL Server, MongoDB, Angular, Angular Material, ngrx, rxjs, Auth0
    
       </p> -->
-      <div class="mt-5 max-w-md mx-auto sm:flex sm:justify-center md:mt-8">
-        <div class="rounded-md shadow">
-          <a href="#" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10">
+      <!-- <div class="mt-5 max-w-md mx-auto sm:flex sm:justify-center md:mt-8 flex-col">
+      <div class="rounded-md shadow-lg">
+          <a href="mailto:martin@nuages.org" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10">
             Contactez moi!
           </a>
-        </div>
-      
-      </div>
+       
+        </div> 
+
+        
+      </div> -->
+
+      <p class="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl flex flex-col ">
+        <span class="font-bold">Martin Massé</span>
+        <a href="mailto:martin@nuages.org" class="hover:text-indigo-600">martin@nuages.org</a>
+        <a href="https://www.linkedin.com/in/martinmasse1402/" class="mt-3 hover:text-indigo-600"><i class="fab fa-linkedin fa-2x" ></i> </a>
+      </p>
+
+      <p class="mt-5 max-w-md mx-auto flex justify-center md:mt-8 ">
+        <img src="/static/aws-certified-developer-associate.png" width="200" height="200" alt="" />
+      </p>
     </div>
+
   </main>
--- svelte.config.js
+++ svelte.config.js
@@ -13,7 +13,7 @@ const config = {
 	],
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		//target: '#svelte',
+		target: '#svelte',
 		adapter: adapter({
 			// default options are shown
 			pages: 'build',
