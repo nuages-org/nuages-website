3628aa0fa60b0f172af551988adba961e1c0ad48
Martin Massé <martin@nuages.org>
2021-05-27 08:15:23 -0400

    ok

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,5 @@
 
-<main class="mt-16 mx-auto max-w-7xl px-4 sm:mt-24">
+<main class="mt-32 mx-auto max-w-7xl px-4 sm:mt-32">
     <div class="text-center">
         <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl mb-6">
             <span class="block xl:inline">Nuages.org</span>
@@ -7,13 +7,13 @@
           </h1>
       <h1 class="text-2xl tracking-tight font-extrabold text-gray-900 sm:text-3xl md:text-4xl">
        
-        <span class="block text-indigo-600 xl:inline">Développement Cloud</span>  <span class="block xl:inline">avec .NET Core</span>
+        <span class="block text-indigo-600 xl:inline">Développement Cloud</span>  <span class="block xl:inline"> AWS avec .NET Core</span>
       </h1>
       <p class="mt-5 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-7 md:text-xl md:max-w-3xl">
-        Je fournis des services de développement complets pour la Stack .NET. J'ai 20 ans d'expérience avec ASP.NET/C#/SQL Server et 7 ans de déploiement sur AWS. 
+        Je fournis des services de développement complets pour AWS avec .NET Core et le Front-End de votre choix (Angular, Vue, Svelte). J'ai 20 ans d'expérience avec ASP.NET/C#/SQL Server et 7 ans de déploiement d'applications en production sur AWS. 
       </p>
       <p class="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
-        Je cherche de nouvelles <span class="font-bold">opportunités de partenariat</span>  pour le développement de solutions Cloud.
+        Je cherche de nouvelles <span class="font-bold text-indigo-600">opportunités de partenariat</span>  pour le développement de solutions Cloud.
 
       </p>
       <p class="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
@@ -37,14 +37,16 @@
       </div> -->
 
       <p class="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl flex flex-col ">
-        <span class="font-bold">Martin Massé</span>
+        <span class="font-bold text-indigo-600">Martin Massé</span>
         <a href="mailto:martin@nuages.org" class="hover:text-indigo-600">martin@nuages.org</a>
         <a href="https://www.linkedin.com/in/martinmasse1402/" class="mt-3 hover:text-indigo-600"><i class="fab fa-linkedin fa-2x" ></i> </a>
       </p>
 
-      <p class="mt-5 max-w-md mx-auto flex justify-center md:mt-8 ">
+      
+
+      <!-- <p class="mt-5 max-w-md mx-auto flex justify-center md:mt-8 ">
         <img src="aws-certified-developer-associate.png" width="200" height="200" alt="" />
-      </p>
+      </p> -->
     </div>
 
   </main>
