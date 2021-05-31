e0cafd5cf476ea6d9e15aaf85af97af69300643d
Martin Massé <martin@nuages.org>
2021-05-31 10:28:01 -0400

    ok

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,7 +2,7 @@
 <main class="mt-32 mx-auto max-w-7xl px-4 sm:mt-32">
     <div class="text-center">
         <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl mb-6">
-            <span class="block xl:inline">Nuages.org</span>
+            <span class="block xl:inline">nuages.io</span>
             <span class="block text-indigo-600 xl:inline"></span>
           </h1>
       <h1 class="text-2xl tracking-tight font-extrabold text-gray-900 sm:text-3xl md:text-4xl">
@@ -21,24 +21,9 @@
       </p>
     
    
-      <!-- <p class="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-sm md:mt-5 md:text-lg md:max-w-3xl">
-        ASP.NET, ASP.NET Core, C# , SignalR, EF, MVC, xUnit, Moq, EC2, ELB, EB, S3, SQS, SNS, SES, RDS, Elasticache (REDIS), Lambda, CloudWatch, CloudFront, CodeBuild, CodePipeline, Route 53, API Gateway, SQL Server, MongoDB, Angular, Angular Material, ngrx, rxjs, Auth0
-   
-      </p> -->
-      <!-- <div class="mt-5 max-w-md mx-auto sm:flex sm:justify-center md:mt-8 flex-col">
-      <div class="rounded-md shadow-lg">
-          <a href="mailto:martin@nuages.org" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10">
-            Contactez moi!
-          </a>
-       
-        </div> 
-
-        
-      </div> -->
-
       <div class="mt-12 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-12 md:text-xl md:max-w-3xl flex flex-col ">
         <span class="font-bold text-indigo-600">Martin Massé</span>
-        <a href="mailto:martin@nuages.org" class="hover:text-indigo-600">martin@nuages.org</a>
+        <a href="mailto:martin@nuages.io" class="hover:text-indigo-600">martin@nuages.io</a>
         <div>
           <p class="mt-6 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl flex flex-row justify-center space-x-6 ">
             <a href="https://www.linkedin.com/in/martinmasse1402/" class="mt-3 hover:text-indigo-600"><i class="fab fa-linkedin fa-2x" ></i> </a>
